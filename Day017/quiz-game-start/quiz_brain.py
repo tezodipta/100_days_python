2856f675f0353cc7b1459b5cb2e93fc814e90d1e
tezodipta <tezodipta04@gmail.com>
2024-03-04 12:54:54 +0530

    commiting oop quiz game

---
+++ Day017/quiz-game-start/quiz_brain.py
@@ -0,0 +1,25 @@
+class QuizBrain:
+    def __init__(self,question_list):
+        self.question_number = 0
+        self.question_list = question_list
+        self.score = 0
+
+    def still_has_questions(self):
+        return self.question_number < len(self.question_list)
+
+    def next_question(self):
+        current_question = self.question_list[self.question_number]
+        self.question_number += 1
+
+        user_ans = input(f"Q.{self.question_number}: {current_question.text} (true/false):")
+        self.check_answer(user_ans,current_question.answer)
+
+    def check_answer(self,user_ans,current_answer):
+        if user_ans.lower() == current_answer.lower():
+            print("Right answer")
+            self.score += 1
+            
+        else:
+            print("Wrong answer")
+        print(f"the correct answer is {current_answer}")
+        print(f"your score is {self.score}/{self.question_number}")
