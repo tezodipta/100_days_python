06d605dec7a3489e4be680bd3a06fe161ffcc140
tezodipta <tezodipta04@gmail.com>
2024-04-09 20:17:48 +0530

    day25 final commit

--- Day025/mian.py
+++ Day025/mian.py
@@ -1,8 +1,52 @@
-import csv
-with open("weather_data.csv")as w_file:
-    data = csv.reader(w_file)
-    temp = []
-    for row in data:
-        print(row)
+# import csv
+# with open("weather_data.csv")as w_file:
+#     data = csv.reader(w_file)
+#     temp = []
+#     for row in data:
+#         print(row)
+#         if row[1] != 'temp':
+#             row[1] =  int(row[1])
 
-    
+import pandas as pd
+
+# data = pd.read_csv("weather_data.csv")
+# temp_list = data['temp'].to_list()
+
+# hell no 
+# temp_len = len(temp_list)
+# total_temp = 0
+# for temp in temp_list:
+#     total_temp += tem
+# avg_temp = total_temp/temp_len
+# print(avg_temp)
+
+# avg_temp = sum(temp_list)/len(temp_list)
+# print(avg_temp)
+# print(data['temp'].mean())
+# print(data['temp'].max())
+
+# print(data[data.temp == data.temp.max()])
+
+# monday = data[data.day == 'Monday']
+# monday_temp = int(monday.temp.iloc[0])
+# monday_temp_F = monday_temp * 9/5 + 32
+# print(monday_temp_F)
+
+# # Create a dataframe from scratch
+# data_dict = {
+#     'students': ['Amy', 'James', 'Angela'],
+#     'scores': [76, 56, 65]
+# }
+# data = pd.DataFrame(data_dict)
+# data.to_csv("new_data.csv")
+
+sdata = pd.read_csv("004201~1.CSV")
+Gray_sq_count = (len(sdata[sdata["Primary Fur Color"] == "Gray"]))
+Black_sq_count = (len(sdata[sdata["Primary Fur Color"] == "Black"]))
+Cinnamon_sq_count = (len(sdata[sdata["Primary Fur Color"] == "Cinnamon"]))
+data_dict = {
+    'Fur_Color': ['Gray', 'Black', 'Cinnamon'],
+    'Count': [Gray_sq_count, Black_sq_count, Cinnamon_sq_count]
+}
+df = pd.DataFrame(data_dict)
+df.to_csv("squirrel_color_count.csv")
---
+++ Day025/us-states-game-start/main.py
@@ -0,0 +1,33 @@
+import turtle
+import pandas as pd
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+data = pd.read_csv("50_states.csv")
+data_list = data.state.to_list()
+guessed_state = []
+
+while len(guessed_state) < 50 :
+    answer_state = screen.textinput(title=f"{len(guessed_state)}/50 states Correct", prompt="What's another state's name?").title()
+    if answer_state == "Exit":
+        missing_state = []
+        for state in data_list:
+            if state not in guessed_state:
+                missing_state.append(state)
+        new_data = pd.DataFrame(missing_state)
+        new_data.to_csv("missing_state.csv")
+        break
+    if answer_state in data_list:
+        guessed_state.append(answer_state)
+        t= turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x),int(state_data.y))
+        t.write(answer_state)
+
+
